fd04b6fe491b3c54ba29cfedbd8b6d4be3522c13
Daniela Villacís <dvillacis17@hotmail.com>
2022-12-08 18:01:37 -0500

    AULESTIA VILLACIS 18:01

---
+++ app/src/main/java/fisei/uta/edu/ec/aulestia_villacis_examen_01/SecondActivity.java
@@ -0,0 +1,14 @@
+package fisei.uta.edu.ec.aulestia_villacis_examen_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
